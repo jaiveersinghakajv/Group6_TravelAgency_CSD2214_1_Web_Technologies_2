b1568e3cd74c170bdf4751bef09ec56f1b452316
MerinKezia <keziaabraham401@gmail.com>
2021-04-10 05:10:45 -0400

    JS created for header

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,31 @@ type="text/javascript">
 	height: "toggle", opacity: "toggle"
 	}, "slow");
 });
+
+
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+    $(window).on('load scroll',function(){
+
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+    
+        if($(window).scrollTop() > 30){
+          $('header').addClass('header-active');
+        }else{
+          $('header').removeClass('header-active');
+        }
+    });
+
+
+
+
+
+
+
+
+
